a4e8127509351559f7f473eaf0cee769f0cd8704
Lucia Iramain <luciairamain5@gmail.com>
2022-03-14 11:54:37 -0300

    Se agregaron styled components y se creo el formulario del cotizador

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <div>
+    <Contenedor>
       <Header titulo={'Cotizador de seguros'}/>
-    </div>
+      <ContenedorFormulario>
+        
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Formulario = () => {
+    return (
+        <div>
+            <form>
+                <div>
+                    <label>Marca</label>
+                    <select>
+                        <option value="">-- Seleccione --</option>
+                        <option value="americano">Americano</option>
+                        <option value="europeo">Europeo</option>
+                        <option value="asiatico">Asiatico</option>
+                    </select>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Formulario;
